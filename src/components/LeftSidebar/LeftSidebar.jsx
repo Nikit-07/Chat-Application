614ac3898370b68ae7b80711e2c6c6feeb119fc5
Nikit <nikitbhakuni094@gmail.com>
2024-12-11 16:37:50 +0530

    Added Chat Page Components

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <div>adsfasddsfa
+      
+    </div>
+  )
+}
+
+export default LeftSidebar
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-      this is the chat page
+    <div className="min-h-screen bg-[linear-gradient(#596AFF,#383699)] grid place-items-center">
+      <div className='w-[95%] h-[70vh] max-w-[1000px] grid grid-cols-[1fr_2fr_1fr]  '>
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+
+      </div>
+      
     </div>
   )
 }
