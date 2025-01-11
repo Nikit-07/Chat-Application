d95c75dbe8999f1a0eb6a20df35d824e89ad18cc
Nikit <nikitbhakuni094@gmail.com>
2025-01-11 15:32:58 +0530

    Implement search functionality to display users by username in LeftSidebar component

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../config/firebase';
+import { AppContext } from '../../context/AppContext';
 
 const LeftSidebar = () => {
-    const navigate= useNavigate();
+
+    const navigate = useNavigate();
+    const { userData } = useContext(AppContext);
+    // console.log("Thsi is user logged in data", userData);
+
+    const [user, setUser] = useState(null);
+    const [showSearch, setShowSearch] = useState(false);
+
+    const inputHandler = async (e) => {
+        try {
+            const input = e.target.value;
+
+            if (input) {
+                setShowSearch(true);
+                const userRef = collection(db, 'users');
+                const q = query(userRef, where("username", "==", input.toLowerCase()));
+                const querySnap = await getDocs(q);
+
+                if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+                    // console.log("First Doc Data:", querySnap.docs[0].data()); // Actual data of the first doc
+                    setUser(querySnap.docs[0].data());
+                }
+                else {
+                    setUser(null);
+                }
+
+            }
+            else{
+                setShowSearch(false);
+            }
+
+        } catch (error) {
+            console.error("Error in inputHandler:", error);
+        }
+    }
+
+    console.log("All users data", user);
+
+
     return (
         // left sidebar
         <div className='bg-[#001030] text-white h-[75vh] ' >
@@ -23,7 +64,7 @@ const LeftSidebar = () => {
                         <div className=' absolute top-[100%] right-0 w-[130px] p-[20px] rounded-[5px] bg-white text-black
                         [&>p]:cursor-pointer [&>p]:text-[14px] hidden group-hover:block ' >
 
-                            <p onClick={()=> navigate('/profile')} >Edit Profile</p>
+                            <p onClick={() => navigate('/profile')} >Edit Profile</p>
                             <hr className=' border-none h-px bg-[#a4a4a4] my-2 mx-0 ' />
                             <p  >Logout</p>
                         </div>
@@ -33,7 +74,7 @@ const LeftSidebar = () => {
                 {/* search bar */}
                 <div className='bg-[#002670] flex items-center gap-[10px] py-[10px] px-3 mt-5 ' >
                     <img src={assets.search_icon} alt="search-icon" className='w-4 ' />
-                    <input type="text" placeholder='Search here...'
+                    <input onChange={inputHandler} type="text" placeholder='Search here...'
                         className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] ' />
                 </div>
             </div>
@@ -43,7 +84,34 @@ const LeftSidebar = () => {
 
                 {/* Multiple users */}
 
-                {
+                { showSearch && user ? 
+                    
+                        <div className='flex items-center gap-[10px] py-[10px] px-[20px] cursor-pointer text-[13px] hover:bg-[#077EFF] group'>
+
+                            <img src={user.avatar} alt="profile-img" className='w-[35px] rounded-[50%] aspect-square ' />
+                            <p> {user.name} </p>
+                              
+                        </div>
+                        : 
+                        Array(10).fill("").map((arr, index) => (
+                            <div className='flex items-center gap-[10px] py-[10px] px-[20px] cursor-pointer text-[13px] hover:bg-[#077EFF] group' key={index}>
+    
+                                <img src={assets.profile_img} alt="profile-img" className='w-[35px] rounded-[50%] aspect-square ' />
+    
+                                <div className='flex flex-col'>
+                                    <p>David</p>
+                                    <span className='text-[#9f9f9f] text-[11px] group-hover:text-white '>Hello, How are you.</span>
+                                </div>
+                            </div>
+                        ))
+                    
+                }
+
+
+
+
+
+                {/* {
                     Array(10).fill("").map((arr, index) => (
                         <div className='flex items-center gap-[10px] py-[10px] px-[20px] cursor-pointer text-[13px] hover:bg-[#077EFF] group' key={index}>
 
@@ -55,7 +123,7 @@ const LeftSidebar = () => {
                             </div>
                         </div>
                     ))
-                }
+                } */}
 
             </div>
         </div>
